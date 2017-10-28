5ae1ae3fa4a6c04d3c6ab2d6f33a13db0ab47a65
Tim Leland <timothy.leland@gmail.com>
2017-10-28 12:20:44 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const puppeteer = require('puppeteer');
+const port = process.env.PORT || 8080;
+const validUrl = require('valid-url');
+
+var addHttpToUrl = function(url) {
+    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+        url = 'http://' + url;
+    }
+
+    return url;
+};
+
+app.get('/', function(req, res) {
+    var encryptedUrl = decodeURIComponent(req.query.value);
+    var urlToScreenshot = addHttpToUrl(encryptedUrl);
+
+    var apiKey = req.query.apiKey;
+    if (validUrl.isWebUri(urlToScreenshot)) {
+        console.log('Screenshotting: ' + urlToScreenshot);
+        (async() => {
+            const browser = await puppeteer.launch({
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+            });
+
+            const page = await browser.newPage();
+            await page.goto(urlToScreenshot);
+            await page.screenshot().then(function(buffer) {
+                res.send(buffer)
+            });
+
+            await browser.close();
+        })();
+    } else {
+        res.send('Invalid url: ' + urlToScreenshot);
+    }
+
+});
+
+app.listen(port, function() {
+    console.log('App listening on port ' + port)
+})
