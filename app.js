9a4382fce8e5de90c75949f6f486a5c5d7c59302
abhishek <abhishek7gg7@gmail.com>
2019-07-25 16:26:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,22 +2,8 @@ const express = require('express');
 const app = express();
 const puppeteer = require('puppeteer');
 const port = process.env.PORT || 8080;
-const validUrl = require('valid-url');
-
-var parseUrl = function(url) {
-    url = decodeURIComponent(url)
-    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
-        url = 'http://' + url;
-    }
-
-    return url;
-};
 
 app.get('/', function(req, res) {
-    var urlToScreenshot = parseUrl(req.query.url);
-
-    if (validUrl.isWebUri(urlToScreenshot)) {
-        console.log('Screenshotting: ' + urlToScreenshot);
         (async() => {
             const browser = await puppeteer.launch({
                 args: ['--no-sandbox', '--disable-setuid-sandbox']
@@ -33,12 +19,6 @@ app.get('/', function(req, res) {
 
             await browser.close();
         })();
-    } else {
-        res.send('Invalid url: ' + urlToScreenshot);
-    }
-
 });
 
-app.listen(port, function() {
-    console.log('App listening on port ' + port)
-})
+app.listen(port)
