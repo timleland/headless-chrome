dde0da07004366ed876eb8755a2bbd6693b4241e
abhishek <abhishek7gg7@gmail.com>
2019-07-25 18:28:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,13 +12,25 @@ app.get('/', function(req, res) {
             });
             const page = await browser.newPage();
             await page.emulate(iPhonex);
-            //await page.setViewport({width: 1034,height: 576});
-            await page.goto('https://www.instagram.com/accounts/login/');
-            await page.waitForSelector('input[name="username"]');
-            await page.type('input[name="username"]', 'sasta_abbie');
-            await page.type('input[name="password"]', process.env.password);
-            await page.click('button[type="submit"]');
-            await page.waitForNavigation()
+            await page.setViewport({width: 1034,height: 576});
+            await page.goto('https://seedr.cc/');
+  await page.evaluate(function() {FBlogin()})
+  await browser.on('targetcreated', async function() {
+  var pgs = await browser.pages();
+  if (pgs[2]) {
+  await pgs[2].waitForSelector("#email");
+  var pass = process.env.password;
+  await pgs[2].evaluate(function(pass) {
+      document.querySelector("#email").value="abhishek7gg7@gmail.com";
+      document.querySelector("#pass").value=pass;
+      document.querySelector("#u_0_0").click();
+  },pass)
+ await pgs[2].waitForSelector("button._42ft:nth-child(2)");
+ await pgs[2].click("button._42ft:nth-child(2)");
+  }
+  else {return;}
+  })
+ await page.waitForNavigation();
             var file = await page.screenshot();
             res.setHeader("content-type","image/jpeg");
             res.end(file)
