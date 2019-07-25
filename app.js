0a613e31b09ec1e1f7ea77c23cbd78f62ed864d5
abhishek <abhishek7gg7@gmail.com>
2019-07-25 17:16:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ app.get('/', function(req, res) {
             });
             const page = await browser.newPage();
             await page.setViewport({width: 1034,height: 576});
-            await page.goto('https://m.facebook.com/login.php?skip_api_login=1&api_key=124024574287414&kid_directed_site=0&app_id=124024574287414&signed_next=1&next=https%3A%2F%2Fm.facebook.com%2Fdialog%2Foauth%3Fclient_id%3D124024574287414%26redirect_uri%3Dhttps%253A%252F%252Fwww.instagram.com%252Faccounts%252Fsignup%252F%26state%3D%257B%2522fbLoginKey%2522%253A%2522c21rn4k7mcn91oix9f8yz9386rkivs6jiumxl1pcnvgt1gc1gbd%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252F%2522%257D%26scope%3Demail%26response_type%3Dcode%252Cgranted_scopes%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3D2560069b-bb5d-4233-937a-76542bb05f09&cancel_url=https%3A%2F%2Fwww.instagram.com%2Faccounts%2Fsignup%2F%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3D%257B%2522fbLoginKey%2522%253A%2522c21rn4k7mcn91oix9f8yz9386rkivs6jiumxl1pcnvgt1gc1gbd%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252F%2522%257D%23_%3D_&display=touch&locale=en_GB&refsrc=http%3A%2F%2Fwww.instagram.com%2F&_rdr');
+            await page.goto("https://www.instagram.com/accounts/login/");
+            await page.waitForSelector(".yWX7d")
+            await page.click(".yWX7d")
+            await page.waitForNavigation()
             await page.type("#m_login_email","abhishek7gg7@gmail.com")
             await page.type(".bt",process.env.password)
             await page.click('input.n')
