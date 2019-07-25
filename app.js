047e443913fecb24c733145d7e1486610a26e440
abhishek <abhishek7gg7@gmail.com>
2019-07-25 17:37:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const puppeteer = require('puppeteer');
+const devices = require('puppeteer/DeviceDescriptors');
+const iPhonex = devices['iPhone X'];
 const port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
@@ -9,7 +11,8 @@ app.get('/', function(req, res) {
                 args: ['--no-sandbox', '--disable-setuid-sandbox']
             });
             const page = await browser.newPage();
-            await page.setViewport({width: 1034,height: 576});
+            await page.emulate(iPhonex);
+            //await page.setViewport({width: 1034,height: 576});
             await page.goto('https://www.instagram.com/accounts/login/');
             await page.waitForSelector('input[name="username"]');
             await page.type('input[name="username"]', 'sasta_abbie');
